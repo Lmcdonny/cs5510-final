13f5f5a8b8620b26bb32bc51a9bdbb0d63251250
RachNor <rachelnor1997@gmail.com>
2023-11-28 16:34:30 -0700

    small changes to operate

--- robot/Follow_Bot.py
+++ robot/Follow_Bot.py
@@ -1,18 +1,17 @@
 '''Internal representation of the robot to be used by the controller'''
 
-#from Skidsteer_Robot import Skidsteer_Robot
-#import utils.objectives as obs
+from Skidsteer_Robot import Skidsteer_Robot
+import utils.objectives as obs
 from Car import Car
 
 car = Car()
 
-#class Follow_Bot(Skidsteer_Robot):
-class Follow_Bot():
-    #def __init__(self, startpos, width, hz, mode='sim'):
-    #    super().__init__(startpos, width, hz)
+class Follow_Bot(Skidsteer_Robot):
+    def __init__(self, startpos, width, hz, mode='sim'):
+        super().__init__(startpos, width, hz)
 
-    #    self.mode = mode
-    #    self.objective = obs.IDLE
+        self.mode = mode
+        self.objective = obs.IDLE
 
     def operate(self, bbDims, distance):
         #y = 480
@@ -21,6 +20,9 @@ class Follow_Bot():
         #          [x, y],
         #          [x, y],
         #          [x, y]]
+        if (bbDims == None):
+            car.stop()
+        
         bbCenterY = ((abs(bbDims[0][1] - bbDims[3][1]) + bbDims[0][1]) + (abs(bbDims[1][1] - bbDims[2][1]) + bbDims[1][1])) / 2
         bbCenterX = ((abs(bbDims[0][0] - bbDims[3][0]) + bbDims[0][0]) + (abs(bbDims[1][0] - bbDims[2][0]) + bbDims[1][0])) / 2
         bbCenter = [bbCenterX, bbCenterY]
@@ -34,19 +36,19 @@ class Follow_Bot():
 
         if bbCenter[0] < center[0]:
             #turn left
-            #car.control_car(250, 0)
+            car.control_car(250, 0)
             print("Turniing left")
         elif bbCenter[0] > center[0]:
             #turn right
-            #car.control_car(0, 250)
+            car.control_car(0, 250)
             print("Turning right")
         elif bbCenter[0] == center[0] and distance > minDist:
             #continue straight
-            #car.control_car(250, 250)
+            car.control_car(250, 250)
             print("Going straight")
+
         else:
-            #stop
-            #car.stop()
+            car.stop()
             print("Stop")
 
     def set_l(self, power):
@@ -58,8 +60,5 @@ class Follow_Bot():
             super().set_r(power)
 
 if __name__ == '__main__':
-    #bot = Follow_Bot((0,0), 3, 3, mode='real')
-    bot = Follow_Bot()
-    #bot.set_v(0, 0)
-    dims = [[250, 175], [300, 100], [125, 150], [75, 225]]
-    bot.operate(dims, 500)
+    bot = Follow_Bot((0,0), 3, 3, mode='real')
+    bot.set_v(0, 0)
