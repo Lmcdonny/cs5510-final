3230a1327e4d71637f3fcb2eae07b9a42fd40d99
Liam McDonald <liam@Liams-MBP-2.hsd1.ut.comcast.net>
2023-12-01 20:20:18 -0700

    uncommented change in yolo box logic

--- Yolo.py
+++ Yolo.py
@@ -72,10 +72,9 @@ class Yolo:
         term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
 
         while self.running:
-            # if b != self.yolo_box:
-            #     print("Yolo.py: Change in Yolo box")
-            #     break
-            print("we in there")
+            if b != self.yolo_box:
+                print("Yolo.py: Change in Yolo box")
+                break
             frame = np.ascontiguousarray(self.cam.capture_array()[:, :, 0:3])
             hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
             dst = cv.calcBackProject([hsv],[0],roi_hist,[0,180],1)
